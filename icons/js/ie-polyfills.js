4620737c34769f5ebf267ddaa9dc96778839fa99
varstachok <allexfreesony@gmail.com>
2020-01-21 21:46:48 +0300

    add 'icons' and 'imgsoptim' directories for verstachok youtube channel

---
+++ icons/js/ie-polyfills.js
@@ -0,0 +1,33 @@
+'use strict';
+
+(function () {
+
+  // region NodeList.prototype.forEach
+  if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = Array.prototype.forEach;
+  }
+  // endregion
+
+  // region Element.closest()
+  // https://developer.mozilla.org/en-US/docs/Web/API/Element/closest#Polyfill
+  if (!Element.prototype.matches) {
+    Element.prototype.matches = Element.prototype.msMatchesSelector ||
+      Element.prototype.webkitMatchesSelector;
+  }
+
+  if (!Element.prototype.closest) {
+    Element.prototype.closest = function (s) {
+      var el = this;
+
+      do {
+        if (el.matches(s)) {
+          return el;
+        }
+
+        el = el.parentElement || el.parentNode;
+      } while (el !== null && el.nodeType === 1);
+      return null;
+    };
+  }
+  // endregion
+})();
